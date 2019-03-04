2e11062b20907bc0d8e224b3c64235daaff72990
Moustafa Mohsen <moustafamohsen1@gmail.com>
2019-03-04 22:20:20 +0200

    hotscript

--- HotScript.ps1
+++ HotScript.ps1
@@ -1,17 +1,11 @@
-#Moustafa Mohesen 2018
-#HardCoded Variables
+#Moustafa Mohesen 2019
+
+#Set Variables
 $inputcsspath = "C:\Users\moust\Desktop\test-css-to-angular\animate.css"
 $outputtspath = "C:\Users\moust\Desktop\test-css-to-angular\out\"
-#$publishdirectory="C:\Data\Dev\Projects\fridge-notes\fridge-notes\src\app\animations"
-
 
 pause
 node css-to-angular-styles $inputcsspath $outputtspath
-
-
-#Copy-Item $outputtspath -Destination $publishdirectory -Force 
-
-
 
 echo 'all done'
 pause
